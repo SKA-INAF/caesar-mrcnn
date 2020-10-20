dfb0d2a03c2462b8cbe23639ac46e0890f9f7e41
riggi <simone.riggi@gmail.com>
2020-10-20 12:17:14 +0200

    Add absolute path of sed

--- scripts/run_mrcnn.sh
+++ scripts/run_mrcnn.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-## SET PYTHON ENV
-#unset PYTHONPATH
-#export PYTHONPATH=$PYTHONPATH:$MASKRCNN_DIR/lib/python3.6/site-packages/
-#export PATH=$PATH:$MASKRCNN_DIR/bin:$PRMON_DIR/bin
-
-echo "PYTHONPATH=$PYTHONPATH"
-
 #######################################
 ##         CHECK ARGS
 #######################################
@@ -163,160 +155,160 @@ do
     	GRAYIMG_OPTION="--grayimg"
     ;;
 		--classdict=*)
-    	CLASS_DICT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	CLASS_DICT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			CLASS_DICT_OPTION="--classdict=$CLASS_DICT"
     ;;
 		--classdict-model=*)
-    	CLASS_DICT_MODEL=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	CLASS_DICT_MODEL=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			CLASS_DICT_MODEL_OPTION="--classdict_model=$CLASS_DICT_MODEL"
     ;;
 		--remap-classids*)
     	REMAP_CLASSIDS_OPTION="--remap_classids"
     ;;
 		--classid-remap-dict=*)
-    	CLASSID_REMAP_DICT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	CLASSID_REMAP_DICT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			CLASSID_REMAP_DICT_OPTION="--classid_remap_dict=$CLASSID_REMAP_DICT"
     ;;
 		--dataloader=*)
-    	DATALOADER=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	DATALOADER=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			DATALOADER_OPTION="--dataloader=$DATALOADER"
     ;;
 		--datalist=*)
-    	DATALIST=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	DATALIST=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			DATALIST_OPTION="--datalist=$DATALIST"
     ;;
 		--datalist-train=*)
-    	DATALIST_TRAIN=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	DATALIST_TRAIN=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			DATALIST_TRAIN_OPTION="--datalist_train=$DATALIST_TRAIN"
     ;;
 		--datalist-val=*)
-    	DATALIST_VAL=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	DATALIST_VAL=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			DATALIST_VAL_OPTION="--datalist_val=$DATALIST_VAL"
     ;;
 		--datadir=*)
-    	DATADIR=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	DATADIR=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			DATADIR_OPTION="--datadir=$DATADIR"
     ;;
 		--validation-data-fract=*)
-    	VAL_DATA_FRACT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	VAL_DATA_FRACT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			VAL_DATA_FRACT_OPTION="--validation_data_fract=$VAL_DATA_FRACT"
     ;;
 		--maxnimgs=*)
-    	MAXNIMGS=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	MAXNIMGS=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			MAXNIMGS_OPTION="--maxnimgs=$MAXNIMGS"
     ;;
 	
 		# - NETWORK ARCHITECTURE
 		--weights=*)
-    	WEIGHTS=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	WEIGHTS=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			WEIGHTS_OPTION="--weights=$WEIGHTS"
     ;;
 		--backbone=*)
-    	BACKBONE=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	BACKBONE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			BACKBONE_OPTION="--backbone=$BACKBONE"
     ;;
 		--backbone-strides=*)
-    	BACKBONE_STRIDES=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	BACKBONE_STRIDES=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			BACKBONE_STRIDES_OPTION="--backbone_strides=$BACKBONE_STRIDES"
     ;;
 
 		# - TRAIN OPTIONS
 		--nepochs=*)
-    	NEPOCHS=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	NEPOCHS=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			NEPOCHS_OPTION="--nepochs=$NEPOCHS"
     ;;
 		--epoch-length=*)
-    	EPOCH_LENGTH=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	EPOCH_LENGTH=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			EPOCH_LENGTH_OPTION="--epoch_length=$EPOCH_LENGTH"
     ;;
 		--nvalidation-steps=*)
-    	NVAL_STEPS=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	NVAL_STEPS=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			NVAL_STEPS_OPTION="--nvalidation_steps=$NVAL_STEPS"
     ;;
 		--rpn-anchor-scales=*)
-    	RPN_ANCHOR_SCALES=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RPN_ANCHOR_SCALES=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			RPN_ANCHOR_SCALES_OPTION="--rpn_anchor_scales=$RPN_ANCHOR_SCALES"
     ;;	
 		--max-gt-instances=*)
-    	MAX_GT_INSTANCES=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	MAX_GT_INSTANCES=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			MAX_GT_INSTANCES_OPTION="--max_gt_instances=$MAX_GT_INSTANCES"
     ;;
 		
 		--rpn-nms-threshold=*)
-    	RPN_NMS_THRESHOLD=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RPN_NMS_THRESHOLD=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			RPN_NMS_THRESHOLD_OPTION="--rpn_nms_threshold=$RPN_NMS_THRESHOLD"
     ;;
 		--rpn-train-anchors-per-image=*)
-    	RPN_TRAIN_ANCHORS_PER_IMAGE=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RPN_TRAIN_ANCHORS_PER_IMAGE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			RPN_TRAIN_ANCHORS_PER_IMAGE_OPTION="--rpn_train_anchors_per_image=$RPN_TRAIN_ANCHORS_PER_IMAGE"
     ;;
 
 		--train-rois-per-image=*)
-    	RPN_TRAIN_ROIS_PER_IMAGE=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RPN_TRAIN_ROIS_PER_IMAGE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			RPN_TRAIN_ROIS_PER_IMAGE_OPTION="--train_rois_per_image=$RPN_TRAIN_ROIS_PER_IMAGE"
     ;;
 
 		--rpn-anchor-ratios=*)
-    	RPN_ANCHOR_RATIOS=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RPN_ANCHOR_RATIOS=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			RPN_ANCHOR_RATIOS_OPTION="--rpn_anchor_ratios=$RPN_ANCHOR_RATIOS"
     ;;
 
 		--rpn-class-loss-weight=*)
-    	RPN_CLASS_LOSS_WEIGHT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`	
+    	RPN_CLASS_LOSS_WEIGHT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`	
 			RPN_CLASS_LOSS_WEIGHT_OPTION="--rpn_class_loss_weight=$RPN_CLASS_LOSS_WEIGHT"
     ;;
 		--rpn-bbox-loss-weight=*)
-    	RPN_BBOX_LOSS_WEIGHT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RPN_BBOX_LOSS_WEIGHT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			RPN_BBOX_LOSS_WEIGHT_OPTION="--rpn_bbox_loss_weight=$RPN_BBOX_LOSS_WEIGHT"
     ;;
 		--mrcnn-class-loss-weight=*)
-    	MRCNN_CLASS_LOSS_WEIGHT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	MRCNN_CLASS_LOSS_WEIGHT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			MRCNN_CLASS_LOSS_WEIGHT_OPTION="--mrcnn_class_loss_weight=$MRCNN_CLASS_LOSS_WEIGHT"
     ;;
 		--mrcnn-bbox-loss-weight=*)
-    	MRCNN_BBOX_LOSS_WEIGHT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	MRCNN_BBOX_LOSS_WEIGHT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			MRCNN_BBOX_LOSS_WEIGHT_OPTION="--mrcnn_bbox_loss_weight=$MRCNN_BBOX_LOSS_WEIGHT"
     ;;
 		--mrcnn-mask-loss-weight=*)
-    	MRCNN_MASK_LOSS_WEIGHT=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	MRCNN_MASK_LOSS_WEIGHT=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			MRCNN_MASK_LOSS_WEIGHT_OPTION="--mrcnn_mask_loss_weight=$MRCNN_MASK_LOSS_WEIGHT"
     ;;
 	
 		# - TEST OPTIONS
 		--scoreThr=*)
-    	SCORE_THR=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	SCORE_THR=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			SCORE_THR_OPTION="--scoreThr=$SCORE_THR"
     ;;
 		--iouThr=*)
-    	IOU_THR=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	IOU_THR=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			IOU_THR_OPTION="--iouThr=$IOU_THR"
     ;;
 
 		# - DETECT OPTIONS
 		--image=*)
-    	IMAGE=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	IMAGE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			IMAGE_OPTION="--image=$IMAGE"
     ;;
 
 		# - RUN OPTIONS
 		--logs=*)
-    	LOG_DIR=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	LOG_DIR=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			LOGDIR_OPTION="--logs=$LOG_DIR"
     ;;
 		--nthreads=*)
-    	NTHREADS=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	NTHREADS=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			NTHREADS_OPTION="--nthreads=$NTHREADS"
     ;;
 		--ngpu=*)
-    	NGPU=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	NGPU=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			NGPU_OPTION="--ngpu=$NGPU"
     ;;
 		--nimg-per-gpu=*)
-    	NIMG_PER_GPU=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	NIMG_PER_GPU=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
 			NIMG_PER_GPU_OPTION="nimg_per_gpu=$NIMG_PER_GPU"
     ;;
 		--runmode=*)
-    	RUNMODE=`echo $item | sed 's/[-a-zA-Z0-9]*=//'`
+    	RUNMODE=`echo $item | /bin/sed 's/[-a-zA-Z0-9]*=//'`
     ;;
 
     *)
